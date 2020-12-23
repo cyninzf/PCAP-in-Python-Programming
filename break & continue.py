3ad381639db62b9749628b32d067fa9b56b35db0
Cynxinzf <73307720+cyninzf@users.noreply.github.com>
2020-12-23 16:34:44 -0500

    Add files via upload

---
+++ break & continue.py
@@ -0,0 +1,19 @@
+# break - example
+
+print("The break instruction:")
+for i in range (1, 6):
+    if i == 3:
+        break
+    print("Inside the loop.", i)
+print("Outside the loop.")
+
+# continue - example
+
+print("\nThe continue instruction:")
+for i in range(1, 6):
+    if i == 3:
+        continue
+    print("Inside the loop.", i)
+print("Outside the loop.")
+
+    
