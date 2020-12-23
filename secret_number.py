f1640e70b3957fe2ac340c5ed412f4b2966e523d
Cynxinzf <73307720+cyninzf@users.noreply.github.com>
2020-12-23 16:00:04 -0500

    Add files via upload

---
+++ secret_number.py
@@ -0,0 +1,21 @@
+# A junior magician has picked a secret number. He has hidden it in a variable
+# named secret_number. He wants everyone who run his program to play
+# the Guess the secret number game.
+
+secret_number = 777
+print(
+"""
++================================+
+| Welcome to my game, muggle!    |
+| Enter an integer number        |
+| and guess what number I've     |
+| picked for you.                |
+| So, what is the secret number? |
++================================+
+""")
+a = int(input("Enter a number: "))
+while a != secret_number:
+    print ("Ha ha! You're stuck in my loop!")
+    a = int(input("Enter a number: "))
+print("Well done, muggle! You are free now.")
+    
