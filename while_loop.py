3a7a6b24420a6135e264b9163f05d27b8f381019
Cynxinzf <73307720+cyninzf@users.noreply.github.com>
2020-12-23 15:47:54 -0500

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,27 @@
+# A program that reads a sequence of numbers and counts how many numbers
+# are even and how many numbers are odd.
+# The program terminates when zero is entered.
+
+odd_numbers = 0
+even_numbers = 0
+
+# Read the first number.
+number = int(input("Enter a number or type 0 to stop: "))
+
+# 0 terminates execution.
+while number != 0:
+    # Check if the number is odd.
+    if number % 2 == 1:
+        # Increase the odd_numbers counter.
+        odd_numbers += 1
+    else:
+        # Increase the even_numbers counter.
+        even_numbers += 1
+    # Read the next number.
+    number = int(input("Enter a number or type 0 to stop: "))
+
+print ("Odd numbers count:", odd_numbers)
+print ("Even numbers count:", even_numbers)
+
+
+    
