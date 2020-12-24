b3a4875adb663042042adf95a36a108bfa08bc79
Cynxinzf <73307720+cyninzf@users.noreply.github.com>
2020-12-24 09:15:14 -0500

    Add files via upload

---
+++ 3_2_1_10.py
@@ -0,0 +1,20 @@
+# the Ugly Vowel Eater
+# Prompt the user to enter a word and assign it to the user_word variable.
+
+user_word = input("Enter a word: ")
+user_word = user_word.upper()
+
+for letter in user_word:
+    # Complete the body of the for loop.
+    if letter == "A":
+        continue
+    elif letter == "E":
+        continue
+    elif letter == "I":
+        continue
+    elif letter == "O":
+        continue
+    elif letter == "U":
+        continue
+    else: 
+        print(letter, end="\n")
