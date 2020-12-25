32d58abc81a4abf4ed18f14ca166f340ee175cef
Cynxinzf <73307720+cyninzf@users.noreply.github.com>
2020-12-24 20:14:46 -0500

    Add files via upload

---
+++ 4_3_1_1return.py
@@ -0,0 +1,12 @@
+# Returning a result from a function
+
+def happy_new_year(wishes = True):
+      print("Three...")
+      print("Two.....")
+      print("One.....")
+      if not wishes:
+            return
+      print("Happy new year!")
+
+happy_new_year(False)
+      
